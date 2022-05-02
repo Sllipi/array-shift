20ae137d40d91e3c644f26650723f64c822c4122
Sllipi <Sllipi@SLLIPI->
2022-05-02 23:40:31 +0300

    Добавьте файлы проекта.

---
+++ array shift/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace array_shift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nУкажите число сдвига влево!");
+            int left = int.Parse(Console.ReadLine());
+            for (int i = 0; i < left; ++i)
+            {
+                int arrayLast = array[array.Length - 1];
+                for (int j = array.Length - 1; j > 0; j--)
+                {
+                    array[j] = array[j - 1];
+                    array[0] = arrayLast;
+                }
+            }
+            Console.WriteLine("Новый массив: ");
+            for (int i = 0; i < array.Length; ++i)
+            {
+                Console.Write(" " + array[i]);
+            }
+        }
+    }
+}
+/*Дан массив чисел. 
+ *Нужно его сдвинуть циклически на указанное пользователем значение позиций влево, 
+ *не используя других массивов. 
+ *Пример для сдвига один раз: {1, 2, 3, 4} => {2, 3, 4, 1}*/
